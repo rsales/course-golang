852d40931f3caad1b6770166b229de42c9abbd54
Rafael Sales <rafael.sales93@hotmail.com>
2020-07-17 00:58:29 -0300

    first program.

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,22 @@
+// Programas executáveis iniciam pelo pacode main
+package main
+
+/*
+Os códigos em GO são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// A porta de entrada de um progmama Go é a funçano main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+
+	/*
+		Sobre comentearios...
+
+		1) Priorize código legível e faça comentearios que agrega valor!
+		2) Evite comentearios óbivios
+		3) Durante o curso abuse dos comentários (licença poética)
+	*/
+}
