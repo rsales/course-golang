2d46c5b0ad1df2a1e8e2835510845b8de69cd913
rsales <rafael.sales93@hotmail.com>
2024-02-14 17:19:38 -0300

    Type struct concept.

---
+++ types/struct/struc.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type product struct {
+	name     string
+	price    float64
+	discount float64
+}
+
+// Méthod: function with reciver (receptor)
+func (p product) priceWithDiscount() float64 {
+	return p.price * (1 - p.discount)
+}
+
+func main() {
+	var product1 product
+	product1 = product{
+		name:     "Pen",
+		price:    1.79,
+		discount: 0.05,
+	}
+
+	fmt.Println(product1, product1.priceWithDiscount())
+
+	product2 := product{"Notebook", 1989.90, 0.10}
+
+	fmt.Println(product2, product2.priceWithDiscount())
+}
