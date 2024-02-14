6388b9ae04eb637d4f2e306d5d80edc2484fdaab
rsales <rafael.sales93@hotmail.com>
2024-02-14 12:32:16 -0300

    Function recusrion concept

---
+++ functions/recursion/recursion.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func factorial(n int) (int, error) {
+	switch {
+	case n < 0:
+		return -1, fmt.Errorf("invalid number: %d", n)
+	case n == 0:
+		return 1, nil
+	default:
+		factorialBefore, _ := factorial(n - 1)
+		return n * factorialBefore, nil
+	}
+}
+
+func main() {
+	result, _ := factorial(5)
+	fmt.Println(result)
+
+	_, err := factorial(-4)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// A better solution would be...
+}
